b18988348a707726d81b52e60bdeee6bee22f937
YUHAN <YUHAN@172.18.7.229>
2019-09-24 10:48:01 +0900

    Spring Framework 3주차 - ModelAttribute-2

---
+++ spring3pjt-07-modelAttr2/src/main/java/yuhan/mvc/modelattribute2/UserVO.java
@@ -0,0 +1,28 @@
+package yuhan.mvc.modelattribute2;
+
+public class UserVO {
+
+	private String userName;
+	private String address;
+	private String car;
+	
+	//Getter, Setter
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCar() {
+		return car;
+	}
+	public void setCar(String car) {
+		this.car = car;
+	}
+}
